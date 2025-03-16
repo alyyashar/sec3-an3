df3f70e0accdaa1810dd8db502dc63c93b6040c3
tajamul6 <tajamul676@gmail.com>
2025-03-16 21:46:11 +0400

    update login

---
+++ frontend/app/(auth-pages)/login/page.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Login() {
+  return (
+    <>
+      <title>Login</title>
+      <div className="p-6">
+        Login page
+      </div>
+    </>
+  );
+}
