69b18865b3d94999d04fe319552b55a57270fe69
Alyyashar-O <ali@o.xyz>
2025-03-18 01:35:44 +0400

    modified scanpy v4

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,13 +1,17 @@
-from sqlalchemy.orm import sessionmaker
-from .models import Base
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Ensure this is correctly set in Railway
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# Use `pool_pre_ping=True` to avoid stale connections
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
+# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
