23ea30bb5401b8148453b6587165fec19b35fc4a
Alyyashar-O <ali@o.xyz>
2025-04-07 10:21:09 +0400

    enchanced nexus frontend security portal right tabs v1.0

--- frontend/app/n3xus/security-portal/_components/security-portal/project-list.tsx
+++ frontend/app/n3xus/security-portal/_components/security-portal/project-list.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
 import { Progress } from "@/components/ui/progress";
-import { UploadContractModal } from "../upload-modal"
+import { UploadContractModal } from "../../upload-modal";
 import {
   AlertTriangle,
   CheckCircle,
